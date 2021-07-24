26f0fe94e9310ce84311a8795637a169fc8a0e9a
krajan <krishnaveni07.rajan@gmail.com>
2021-07-24 12:07:34 +0530

    json added

--- src/test/java/StepDefinition/TestRunner.java
+++ src/test/java/StepDefinition/TestRunner.java
@@ -7,6 +7,8 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions (
+		monochrome=true,
+		plugin= {"pretty","html:target/cucumber-html", "json:target/cucumber.json"},
 		features = "src/test/java/features",
 		tags = {"@Simple,@DataTable"},
 		glue = {"StepDefinition"}
