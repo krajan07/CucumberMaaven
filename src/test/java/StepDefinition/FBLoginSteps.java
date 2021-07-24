7fcfaebc33caaf6d458e6c4b243bf7e1b175e32d
krajan <krishnaveni07.rajan@gmail.com>
2021-07-24 12:19:01 +0530

    Commit

--- src/test/java/StepDefinition/FBLoginSteps.java
+++ src/test/java/StepDefinition/FBLoginSteps.java
@@ -12,7 +12,6 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.junit.CucumberOptions;
 
 public class FBLoginSteps {
 	
--- src/test/java/StepDefinition/TestRunner.java
+++ src/test/java/StepDefinition/TestRunner.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
+//Added Json report
 @RunWith(Cucumber.class)
 @CucumberOptions (
 		monochrome=true,
